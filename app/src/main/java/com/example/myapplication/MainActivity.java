8133cdcae7cfdd073cced31392ad7dc9762b80ba
FeiYang Zheng <feiyang_zheng@outlook.com>
2024-03-07 23:05:10 +0100

    add three page of android application

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import android.widget.Button;
+
+import android.view.View;
+
+import android.content.Intent;
+
 import android.util.Log;
 
 import java.io.IOException;
@@ -19,5 +26,18 @@ public class MainActivity extends AppCompatActivity {
         String url = "https://studev.groept.be/api/a23ib2c03/test"; // 替换为你要获取内容的URL
         String content = Geturl.getContent(url);
         //testchange
+
+        Button button = findViewById(R.id.button_go_to_current);
+
+        // 为按钮设置点击监听器
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 创建Intent以启动新的Activity
+                Intent intent = new Intent(MainActivity.this, activity_current_information.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
